3f27f9daebf4188788b53817f9eb8d327ab9fa24
Daniil Blair <30020332@student.toiohomai.ac.nz>
2019-03-13 10:17:07 +1300

    comment

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //My name is Daniil Blair and i am the developer of this program which tells you if thew number entered is odd or even
+            Console.WriteLine("This program will determine if the number entered is a odd or even number");
+            Console.ReadKey();
+
+            Console.WriteLine("Please enter a whole number and i will tell you if it is odd or even");
+            int input = int.Parse(Console.ReadLine());
+
+            if (input % 2 == 0)
+                Console.WriteLine("The num you entered was even");
+            else
+                Console.WriteLine("The num you habe entered was odd");
+            Console.ReadKey();
+       
+        }
+    }
+}
